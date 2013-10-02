cdf2a43dfd31293c2043e050f33d45a4b510ed97
Marc Aubry <maubry@sierrawireless.com>
2013-10-02 11:53:52 +0200

    Retreiving systems from AV

--- app/routes/system.js
+++ app/routes/system.js
@@ -1,26 +1,76 @@
-/**
-* Systems views
-*/
+"use strict";
+/*
+ * GET users listing.
+ */
 
-exports.list = function(req, res) {
-	res.render('systems', {
-		systems : [ {
-			name : "B1",
-			uid : "001",
-			temperature : 45,
-			lastcommdate : 1380210799215
-		}, {
-			name : "B2",
-			uid : "002",
-			temperature : 25,
-			lastcommdate : 1380210799215
-		}, {
-			name : "C1",
-			uid : "003",
-			temperature : 38,
-			lastcommdate : 1380210799215
-		} ]
+var http = require('https');
+var mod = require('forEachAsync');
+
+// get systems
+function getSystemDatas(system, next) {
+
+	var options = {
+		host : 'qa-trunk.airvantage.net',
+		path : '/api/v1/systems/' + system.uid
+				+ '/data?ids=temperature&access_token=?',
+		method : 'GET'
+	};
+	console.log("request: " + options.host + options.path);
+
+	var req = http.request(options, function(res) {
+		res.setEncoding('utf8');
+		res.on('data', function(data) {
+			data = JSON.parse(data);
+			system.temperature = data.temperature[0].value;
+			next();
+		});
 	});
+	req.on('error', function(e) {
+		next();
+	});
+	req.end();
+}
+
+exports.list = function(pagerequest, pageresponse) {
+
+	var systems = [];
+
+	var options = {
+		host : 'qa-trunk.airvantage.net',
+		path : '/api/v1/systems?fields=uid,name,lastCommDate&access_token=?',
+		method : 'GET'
+	};
+	console.log("request: " + options.host + options.path);
+
+	var req = http.request(options, function(res) {
+		res.setEncoding('utf8');
+		res.on('data', function(data) {
+			data = JSON.parse(data);
+			for ( var i = 0; i < data.items.length; i++) {
+				systems.push(data.items[i]);
+			}
+
+			// request all the temperatures
+			mod.forEachAsync(systems, function(next, element, index, array) {
+				getSystemDatas(element, next);
+
+				// then after all of the elements have been handled
+				// the final callback fires to let you know it's all done
+			}).then(function() {
+				console.log('All requests have finished');
+				pageresponse.render('systems', {
+					systems : systems
+				});
+			});
+
+		});
+	});
+
+	req.on('error', function(e) {
+		console.log('Unable to retreive systems list: ' + e.message);
+	});
+	req.end();
+
 };
 
 // exports.list = function(req, res) { res.render('systemdetail', {}); };
