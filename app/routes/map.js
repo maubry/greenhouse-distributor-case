437afe547b455d4e7d013d3ee5805edf8baf6cfa
Gaetan Morice <gaetan.morice@gmail.com>
2013-10-15 14:43:22 +0200

    Add alarm in header on view map

--- app/routes/map.js
+++ app/routes/map.js
@@ -3,36 +3,76 @@
  * GET map
  */
 
-var http = require('https');
+ var http = require('https');
 
-exports.display = function(pagerequest, pageresponse) {
 
-	var systems = [];
+ exports.display = function(pagerequest, pageresponse) {
 
-	var options = {
-		host : 'qa-trunk.airvantage.net',
-		path : '/api/v1/systems?fields=uid,name,data&access_token='+pagerequest.session.access_token,
-		method : 'GET'
-	};
-	console.log("request: " + options.host + options.path);
+ 	var systems = [];
 
-	var req = http.request(options, function(res) {
-		res.setEncoding('utf8');
-		res.on('data', function(data) {
-			data = JSON.parse(data);
-			for ( var i = 0; i < data.items.length; i++) {
-				systems.push(data.items[i]);
-			}
-			pageresponse.render('map', {
-				systems : systems,
-				active : "map"
-			});
-		});
+ 	var options = {
+ 		host : 'qa-trunk.airvantage.net',
+ 		path : '/api/v1/systems?fields=uid,name,data&access_token='+pagerequest.session.access_token,
+ 		method : 'GET'
+ 	};
+ 	console.log("request: " + options.host + options.path);
+
+ 	var req = http.request(options, function(res) {
+ 		res.setEncoding('utf8');
+ 		res.on('data', function(data) {
+ 			data = JSON.parse(data);
+ 			for ( var i = 0; i < data.items.length; i++) {
+ 				systems.push(data.items[i]);
+ 			}
+
+
+
+
+
+ 			var options = {
+ 				host : 'qa-trunk.airvantage.net',
+ 				path : '/api/v1/alerts?fields=uid,date,target,acknowledged&access_token='+pagerequest.session.access_token,
+ 				method : 'GET'
+ 			};
+ 			console.log("request: " + options.host + options.path);
+ 			http.request(options, function(res) {
+ 				res.setEncoding('utf8');
+ 				res.on('data', function(data) {
+
+ 					console.log( 'Alerts request: ' + data );
+ 					data = JSON.parse(data);
+					// add alert to the system
+					for ( var i = 0; i < data.items.length; i++) {
+						for ( var j = 0; j < systems.length; j++){
+							if (data.items[i].target === systems[j].uid){
+								//create alarm node if needed
+								if (!("alerts" in systems[j])){
+									systems[j].alerts = [];
+								}
+								systems[j].alerts.push(data.items[i]);
+							}
+						}
+					}
+
+					pageresponse.render('map', {
+						alerts_count: data.count,
+						systems : systems,
+						active : "map"
+					});
+					
+				});
+
+ 			}).on('error', function(e) {
+ 				console.log("Unable to get alarms status");
+ 			}).end();
+
+ 		});
 	});
 
 	req.on('error', function(e) {
 		console.log('Unable to retreive systems list: ' + e.message);
 	});
+
 	req.end();
 
 };
