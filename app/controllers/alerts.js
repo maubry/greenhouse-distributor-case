49ab537ab5ea9a953cf3181d58e65ddd323099a1
Gaetan Morice <gaetan.morice@gmail.com>
2013-10-24 14:10:55 +0200

    Use AirVantage model in the alerts view

--- app/controllers/alerts.js
+++ app/controllers/alerts.js
@@ -3,53 +3,49 @@
  */
 "use strict";
 var airvantage = require('../model/airvantage');
-var https = require('https');
+var _ = require('underscore');
+var async = require('async');
 
-exports.get = function(request, pageresponse) {
 
-    /*
-     * Fetch all alarms
-     */
-    var options = {
-        method : 'GET',
-        host : 'qa-trunk.airvantage.net',
-        path : '/api/v1/alerts?fields=uid,date,target,targetDescription,rule,acknowledgedBy&access_token=' + request.session.access_token,
-    };
-    console.log('Alerts request: ' + options.host + options.path);
 
-    // Send request
-    https.request(options, function(response) {
-        response.setEncoding('utf8');
-        response.on('data', function(data) {
-            console.log('Alerts data: '+ data);
-            data = JSON.parse(data);
-            var alerts_count = 0;
-            for(var a=0; a < data.items.length; a++){
-                var alert = data.items[a];
-                if (!alert.acknowledgedBy){
-                    alerts_count++;
-                }
-            }
-            pageresponse.render('alerts', {
-                active : 'alerts',
-                alerts : data.items,
+exports.get = function(req, resp) {
+
+    //request alerts from AirVantage
+    airvantage.alerts_query({"access_token": req.session.access_token})(function(err, res) {
+        if (err) {
+            console.log("ERR: " + err);
+        } else {
+
+            // alerts
+            var alerts = res.items;
+
+            // count alert not aknowledged
+            var alerts_count = _.size(_.reject(alerts, function(alert){return alert.acknowledgedBy}));
+
+            // render the page
+            resp.render('alerts', {
+                alerts : alerts,
+                active : "alerts",
                 alerts_count : alerts_count
             });
-        }).on('error', function(e) {
-            console.log('Unable to get alarms details: ' + e.message);
-        });
-    }).on('error', function(e) {
-        console.log('Unable to fetch alarms: ' + e.message);
-    }).end();
+        }
+    });
+
 };
 
-exports.post = function(req, res, next){
-    airvantage.alerts_ack({uid: req.body.uid, "access_token": req.session.access_token}, "")(function(error, results){
-        if(error){
-            next(error);
-        }else{
-            res.redirect(req.session.originalUrl);
-        }
 
+
+
+exports.post = function(req, resp, next){
+
+    //post ack to AirVantage
+    airvantage.alerts_ack({uid: req.body.uid, "access_token": req.session.access_token}, "")(function(err, res){
+        if(err){
+            next(err);
+        }else{
+            // redirect to the page that send the ack
+            resp.redirect(req.session.originalUrl);
+        }
     });
+
 };
